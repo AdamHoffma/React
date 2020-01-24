269938d33863a6e65b4de236ce8386c42d9a20f8
Adam Hoffman <wintros@yahoo.com>
2020-01-24 13:32:04 -0700

    why wont images show

--- adam/src/App.js
+++ adam/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h2>A project coming soon</h2>
-      <img src="C:\Users\wintr\Desktop\React\adam\src\images\PHI.png" alt="A phillies banner" height='42' width='42'></img>
+      <div>
+      <img src="C:/Users/wintr/Downloads/SF.png" alt="A SF banner" height='42' width='42'/>
+      </div>
     </div>
   );
 }
