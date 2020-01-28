930b3f786d30860d7d4aa23f96d16adb85089b64
Adam Hoffman <wintros@yahoo.com>
2020-01-27 18:29:22 -0700

    need to add in form submition functionality

--- adam/src/App.js
+++ adam/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Homepage from './components/home.js'
+import UpdateStats from './components/update_stats.js'
 
 function App() {
   return (
     <div className="App">
       <Homepage />
+      <UpdateStats />
     </div>
   );
 }
--- adam/src/components/update_stats.js
+++ adam/src/components/update_stats.js
@@ -1,8 +1,8 @@
 import React, {useState, useEffect} from 'react'
 
-const Update_Stats = () => {
-    const [stats, setStats] = useState([
-        Stats = {
+const UpdateStats = () => {
+    const [stats, setStats] = useState()
+        const Stats = [{
             Name: "Roxie Albert",
             Avg: "",
             OBP: "",
@@ -14,17 +14,223 @@ const Update_Stats = () => {
             HRs: "",
             Rs: "",
             RBIs: "",
-            SBs: ""
-        }
+            SBs: "",
+            Walks: ""
+        },
+        {
+            Name: "Maggie Philpott",
+            Avg: "",
+            OBP: "",
+            SLG: "",
+            Hits: "",
+            ABs: "",
+            Doubles: "",
+            Triples: "",
+            HRs: "",
+            Rs: "",
+            RBIs: "",
+            SBs: "",
+            Walks: ""
+        },
+        {
+            Name: "Annika Spilde",
+            Avg: "",
+            OBP: "",
+            SLG: "",
+            Hits: "",
+            ABs: "",
+            Doubles: "",
+            Triples: "",
+            HRs: "",
+            Rs: "",
+            RBIs: "",
+            SBs: "",
+            Walks: ""
+        },
+        {
+            Name: "Avery Coover",
+            Avg: "",
+            OBP: "",
+            SLG: "",
+            Hits: "",
+            ABs: "",
+            Doubles: "",
+            Triples: "",
+            HRs: "",
+            Rs: "",
+            RBIs: "",
+            SBs: "",
+            Walks: ""
+        },
+        {
+            Name: "Brianna Williams",
+            Avg: "",
+            OBP: "",
+            SLG: "",
+            Hits: "",
+            ABs: "",
+            Doubles: "",
+            Triples: "",
+            HRs: "",
+            Rs: "",
+            RBIs: "",
+            SBs: "",
+            Walks: ""
+        },
+        {
+            Name: "Caitlin Anderson",
+            Avg: "",
+            OBP: "",
+            SLG: "",
+            Hits: "",
+            ABs: "",
+            Doubles: "",
+            Triples: "",
+            HRs: "",
+            Rs: "",
+            RBIs: "",
+            SBs: "",
+            Walks: ""
+        },
+        {
+            Name: "Charlotte Commons",
+            Avg: "",
+            OBP: "",
+            SLG: "",
+            Hits: "",
+            ABs: "",
+            Doubles: "",
+            Triples: "",
+            HRs: "",
+            Rs: "",
+            RBIs: "",
+            SBs: "",
+            Walks: ""
+        },
+        {
+            Name: "Jacey Lane",
+            Avg: "",
+            OBP: "",
+            SLG: "",
+            Hits: "",
+            ABs: "",
+            Doubles: "",
+            Triples: "",
+            HRs: "",
+            Rs: "",
+            RBIs: "",
+            SBs: "",
+            Walks: ""
+        },
+        {
+            Name: "Jaidyn McNeal",
+            Avg: "",
+            OBP: "",
+            SLG: "",
+            Hits: "",
+            ABs: "",
+            Doubles: "",
+            Triples: "",
+            HRs: "",
+            Rs: "",
+            RBIs: "",
+            SBs: "",
+            Walks: ""
+        },
+        {
+            Name: "Kathryn Duncan",
+            Avg: "",
+            OBP: "",
+            SLG: "",
+            Hits: "",
+            ABs: "",
+            Doubles: "",
+            Triples: "",
+            HRs: "",
+            Rs: "",
+            RBIs: "",
+            SBs: "",
+            Walks: ""
+        },
+        {
+            Name: "Mikenna Hoffman",
+            Avg: "",
+            OBP: "",
+            SLG: "",
+            Hits: "",
+            ABs: "",
+            Doubles: "",
+            Triples: "",
+            HRs: "",
+            Rs: "",
+            RBIs: "",
+            SBs: "",
+            Walks: ""
+        },
+        {
+            Name: "Reese Wagnon",
+            Avg: "",
+            OBP: "",
+            SLG: "",
+            Hits: "",
+            ABs: "",
+            Doubles: "",
+            Triples: "",
+            HRs: "",
+            Rs: "",
+            RBIs: "",
+            SBs: "",
+            Walks: ""
+        },
+        {
+            Name: "Sierra Sikich",
+            Avg: "",
+            OBP: "",
+            SLG: "",
+            Hits: "",
+            ABs: "",
+            Doubles: "",
+            Triples: "",
+            HRs: "",
+            Rs: "",
+            RBIs: "",
+            SBs: "",
+            Walks: ""
+        }]
         
-    ])
+    
+
+    useEffect(() => {
+        setStats(Stats)
+    }, [])
+
+    console.log(stats)
+
+    return (
+        <div>
+            {Stats.map(each => {
+                return (
+                    <div>
+                        <p>{each.Name}</p>
+                    <form>                        
+                        <input type="text" placeholder="walks"></input>                        
+                        <input type="text" placeholder="Hits"></input>
+                        <input type="text" placeholder="Abs"></input>
+                        <input type="text" placeholder="Doubles"></input>
+                        <input type="text" placeholder="Triples"></input>
+                        <input type="text" placeholder="HRs"></input>
+                        <input type="text" placeholder="Rs"></input>
+                        <input type="text" placeholder="RBIs"></input>
+                        <input type="text" placeholder="SBs"></input>
+                        <button>Submit</button>                        
+                    </form>
+                    </div>
+                )
+            })}
+            
+        </div>
+    )
 }
 
-export const Update_Stats
+export default UpdateStats
 
-var person = {
-    firstName: "John",
-    lastName: "Doe",
-    age: 50,
-    eyeColor: "blue"
-  };
